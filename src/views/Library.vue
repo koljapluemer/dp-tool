61e7ee1e6479391243f1a277c380ce46f8fdeac3
blackbrokkoli <kastomat@gmail.com>
2023-12-14 17:47:51 +0200

    enhance new library mockup

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -4,13 +4,19 @@ import { ref } from "vue";
 
 const store = useCoreStore();
 
-const topics = ref([])
+const topics = ref([]);
 </script>
 
 <template>
-What do you want to get good at?
+  What do you want to get good at?
+
+  <!-- if topics not empty, show link list of topics; if empty, prompt for a new topic -->
+  <div class="card">
+    <input type="text" class="input" />
+  </div>
+  <div class="card">
+    <h2 class="font-bold">Your Topics</h2>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
